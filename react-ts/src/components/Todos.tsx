226c7dc81b4991dd6010ec85e1d3e82ef6bd82ed
Pannu786 <thepannu786@gmail.com>
2022-06-10 22:35:13 +0530

    Added Todo cmp with TS

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,10 @@
+const Todos: React.FC<{ items: string[] }> = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+export default Todos;
